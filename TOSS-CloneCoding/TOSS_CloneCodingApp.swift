26b8f8791b8f8acd4b20dbead04be9c186589b23
italsehf <alsehfdl0316@naver.com>
2022-09-27 16:40:16 +0900

    Docs: .gitignore 파일 추가

---
+++ TOSS-CloneCoding/TOSS_CloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOSS_CloneCodingApp.swift
+//  TOSS-CloneCoding
+//
+//  Created by Keum MinSeok on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct TOSS_CloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
